ed481c1b62ff01f4b44dcbe4cd5a635196f3ee34
moenuma <moenumasawa@gmail.com>
2024-05-19 01:32:58 -0700

    Rename app

---
+++ JPLearningiOS/JPLearningiOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  JPLearningiOSApp.swift
+//  JPLearningiOS
+//
+//  Created by Numasawa, Moe on 2024-05-19.
+//
+
+import SwiftUI
+
+@main
+struct JPLearningiOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
