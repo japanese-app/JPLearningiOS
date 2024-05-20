d2ec8f43d2ea31d5fcee227b4927842c5fe61933
moenuma <moenumasawa@gmail.com>
2024-05-19 22:09:10 -0700

    Rename RootView

---
+++ JPLearningiOS/RootView.swift
@@ -0,0 +1,37 @@
+//
+//  RootView.swift
+//  JPLearningiOS
+//
+//  Created by Numasawa, Moe on 2024-05-19.
+//
+
+import SwiftUI
+
+struct RootView: View {
+    @State var selectedTab: Tabs = .browse
+    var body: some View {
+        NavigationView {
+            VStack {
+                TabView(selection: $selectedTab) {
+                    BrowseView()
+                        .tag(Tabs.browse)
+                    MylistView()
+                        .tag(Tabs.mylist)
+                    LearningView()
+                        .tag(Tabs.learning)
+                    MediaView()
+                        .tag(Tabs.media)
+                    JournalView()
+                        .tag(Tabs.journal)
+                }
+                
+                CustomTabBar(selectedTab: $selectedTab)
+            }
+            .navigationBarHidden(true)
+        }
+    }
+}
+
+#Preview {
+    RootView()
+}
